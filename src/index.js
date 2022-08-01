7bad522949ed2bda4f4379b527ce789f622189c0
nicolas-baranovskiy <nicolas.baranovskiy@gmail.com>
2022-08-01 19:46:16 +0300

    feat: update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,21 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let sum = [];
+  if (matrix == null) {
+    return sum;
+  }
+  
+  for (let key of matrix) {
+    if (matrix.indexOf(key) % 2 != 0) {
+      key.reverse()
+    }
+  }
+
+  for (let i = 0; i < matrix.length; i++ ){
+    for (let n = 0; n < matrix[i].length; n++) {
+      sum.push(matrix[i][n]);
+    }
+  }
+  return sum;
 }
